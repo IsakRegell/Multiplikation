cdae7763c7b0b90320e9a61d2b103108b7f6aa31
IsakRegell <isak.regell@gmail.com>
2024-09-30 14:24:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Multiplikation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ett nummer");
+            int siffran = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{siffran} * {i} = {siffran * i}");
+            }
+            
+        }
+    }
+}
